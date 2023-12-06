dff0ec0bd8e5a9d4a1d14cf9cbeef40665562b6a
Andreas Kull <akullpp@gmail.com>
2023-12-06 15:24:36 +0100

    Day 1 Part 2

--- go/1.go
+++ go/1.go
@@ -7,11 +7,13 @@ import (
 	"os"
 	"regexp"
 	"strconv"
+	"strings"
 )
 
 // Observations:
 // - there's at least one number per line
-// - if there's only one, it is used twice
+// - there's never a 0
+// - if there's only one number, it is used twice
 func d1p1(filename string) int {
 	file, err := os.Open(filename)
 	if err != nil {
@@ -21,10 +23,50 @@ func d1p1(filename string) int {
 	scanner := bufio.NewScanner(file)
 
 	sum := 0
-	re := regexp.MustCompile("[0-9]")
+	re := regexp.MustCompile("[1-9]")
 	for scanner.Scan() {
 		line := scanner.Text()
 		xs := re.FindAllString(line, -1)
+		s := fmt.Sprintf("%s%s", xs[0], xs[len(xs)-1])
+		n, _ := strconv.Atoi(s)
+		sum += n
+	}
+	fmt.Println("Sum:", sum)
+	return sum
+}
+
+// Only difference to d1p1 is line 49-60 and 63-68
+func d1p2(filename string) int {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+
+	sum := 0
+	re := regexp.MustCompile("[1-9]")
+	// Trick for overlap, e.g. eightwone -> e8twone -> e8t2one -> e8t2o1e
+	replacer := strings.NewReplacer(
+		"one", "o1e",
+		"two", "t2o",
+		"three", "t3e",
+		"four", "f4r",
+		"five", "f5e",
+		"six", "s6x",
+		"seven", "s7n",
+		"eight", "e8t",
+		"nine", "n9e",
+	)
+	for scanner.Scan() {
+		line := scanner.Text()
+		before := line
+		after := replacer.Replace(before)
+		for before != after {
+			before = after
+			after = replacer.Replace(before)
+		}
+		xs := re.FindAllString(after, -1)
 		s := fmt.Sprintf("%s%s", xs[0], xs[len(xs)-1])
 		n, _ := strconv.Atoi(s)
 		sum += n
--- go/1_test.go
+++ go/1_test.go
@@ -15,7 +15,27 @@ func TestExampleD1P1(t *testing.T) {
 func TestInputD1P1(t *testing.T) {
 	expectation := 54916
 
-	result := d1p1("input/d1p1.input")
+	result := d1p1("input/d1.input")
+
+	if result != expectation {
+		t.Errorf("Expected %d, got %d", expectation, result)
+	}
+}
+
+func TestExampleD1P2(t *testing.T) {
+	expectation := 281
+
+	result := d1p2("input/d1p2.example")
+
+	if result != expectation {
+		t.Errorf("Expected %d, got %d", expectation, result)
+	}
+}
+
+func TestInputD1P2(t *testing.T) {
+	expectation := 54728
+
+	result := d1p2("input/d1.input")
 
 	if result != expectation {
 		t.Errorf("Expected %d, got %d", expectation, result)
