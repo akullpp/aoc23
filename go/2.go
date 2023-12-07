a168426a6e4125b618a62a08fc6520b70cac2f3d
Andreas Kull <akullpp@gmail.com>
2023-12-07 10:12:28 +0100

    Day 2 Part 2

--- go/2.go
+++ go/2.go
@@ -43,3 +43,38 @@ func d2p1(filename string) int {
 	}
 	return result
 }
+
+func d2p2(filename string) int {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+
+	result := 0
+	re := regexp.MustCompile(`(\d+)\s(\w+)`)
+	for scanner.Scan() {
+		line := scanner.Text()
+		config := map[string]int{
+			"red":   0,
+			"green": 0,
+			"blue":  0,
+		}
+		for _, m := range re.FindAllStringSubmatch(line, -1) {
+			color := m[2]
+			value, _ := strconv.Atoi(m[1])
+
+			if value > config[color] {
+				config[color] = value
+			}
+		}
+
+		power := 1
+		for _, v := range config {
+			power *= v
+		}
+		result += power
+	}
+	return result
+}
--- go/2_test.go
+++ go/2_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestExampleD2P1(t *testing.T) {
 	expectation := 8
 
-	result := d2p1("input/d2p1.example")
+	result := d2p1("input/d2.example")
 
 	if result != expectation {
 		t.Errorf("Expected %d, got %d", expectation, result)
@@ -16,6 +16,26 @@ func TestInputD2P1(t *testing.T) {
 	expectation := 2685
 
 	result := d2p1("input/d2.input")
+
+	if result != expectation {
+		t.Errorf("Expected %d, got %d", expectation, result)
+	}
+}
+
+func TestExampleD2P2(t *testing.T) {
+	expectation := 2286
+
+	result := d2p2("input/d2.example")
+
+	if result != expectation {
+		t.Errorf("Expected %d, got %d", expectation, result)
+	}
+}
+
+func TestInputD2P2(t *testing.T) {
+	expectation := 83707
+
+	result := d2p2("input/d2.input")
 
 	if result != expectation {
 		t.Errorf("Expected %d, got %d", expectation, result)
